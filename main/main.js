5887f3fbe3d7d0027429be588a9df22945e8cd35
Fangxiang Zou <fxzou@thoughtworks.com>
2018-09-06 20:32:08 +0800

    should return 1A3B when different three number for seq1 with seq2

--- main/main.js
+++ main/main.js
@@ -6,7 +6,12 @@ function guess(seq1, seq2) {
     if (seq1 === seq2) {
         return '4A0B';
     }
-    return '2A2B';
+    let diffCount = 0;
+    let eqCount = 0;
+    for (let i = 0; i < seq1.length; i++) {
+        seq1.charAt(i) === seq2.charAt(i) ? eqCount++ : diffCount++;
+    }
+    return `${eqCount}A${diffCount}B`;
 }
 
 module.exports = {
