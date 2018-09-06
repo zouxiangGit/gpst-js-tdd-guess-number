410ded689bb4314469fbf95db2cd350d9be6580d
Fangxiang Zou <fxzou@thoughtworks.com>
2018-09-06 20:21:13 +0800

    should return 4A0B when seq1 equals seq2

--- main/main.js
+++ main/main.js
@@ -1,6 +1,12 @@
 // Write your cade below:
-function main() {};
+function main() {
+}
+
+function guess(seq1, seq2) {
+    return '4A0B';
+}
 
 module.exports = {
-    main
+    main,
+    guess
 }
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,7 @@
-const main = require('../main/main');
+const app = require('../main/main');
 
 describe('main()', () => {
-
+    it('should return 4A0B when seq1 equals seq2', () => {
+        expect(app.guess('1234','1234')).toEqual('4A0B')
+    })
 });
